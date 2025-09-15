df001b3e9791d7306519344ee9401f3c6ee77cdf
Pedro Candelaresi <pcandelaresi.nqn@gmail.com>
2025-09-15 12:31:26 -0300

    sigue el bardo del login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,7 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 console.log('🚀 [LOGIN] BACKEND_URL:', BACKEND_URL);
+console.log('🚀 [LOGIN] process.env.NEXT_PUBLIC_BACKEND_URL:', process.env.NEXT_PUBLIC_BACKEND_URL);
+console.log('🚀 [LOGIN] process.env.BACKEND_URL:', process.env.BACKEND_URL);
+
 export async function POST(req: NextRequest) {
   if (!BACKEND_URL) {
     return new NextResponse('BACKEND_URL no configurada', { status: 500 });
