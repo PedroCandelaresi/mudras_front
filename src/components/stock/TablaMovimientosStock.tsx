6232ac89449991f72d584a70a2aaedbdd4dba52e
Pedro Candelaresi <pcandelaresi.nqn@gmail.com>
2026-02-20 09:18:25 -0300

    tabla movimientos stock

--- src/components/stock/TablaMovimientosStock.tsx
+++ src/components/stock/TablaMovimientosStock.tsx
@@ -1,7 +1,7 @@
 'use client';
 /* eslint-disable @next/next/no-img-element */
 
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import {
   Box,
   Typography,
